2bf377d3f1e16bb437d11eafd8d171d567aee8c1
ronmnm <spellbound.2808@gmail.com>
2020-06-29 15:10:38 +0300

    fix

--- functions/firebase.js
+++ functions/firebase.js
@@ -8,6 +8,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     "private_key": process.env.FIREBASE_PRIVATE_KEY,
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "project_id": process.env.FIREBASE_PROJECT_ID
   }),
   databaseURL: "https://tbtc-bot-143f1.firebaseio.com"
 });
